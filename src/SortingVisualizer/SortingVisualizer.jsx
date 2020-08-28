13ae80a8a3232d61198cbb84cd430dfc1cdfec08
Manmohan Labh <65655561+labhmanmohan25@users.noreply.github.com>
2020-08-29 01:46:16 +0530

    Update SortingVisualizer.jsx

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -199,7 +199,7 @@ export default class SortingVisualizer extends React.Component {
   }
 
   heapSort() {
-    // We leave it as an exercise to the viewer of this code to implement this method.
+    
   }
 
   render() {
@@ -219,7 +219,7 @@ export default class SortingVisualizer extends React.Component {
         <button onClick={() => this.resetArray()}>Generate New Array</button>
         <button onClick={() => this.mergeSort()}>Merge Sort</button>
         <button onClick={() => this.quickSort()}>Quick Sort</button>
-        <button onClick={() => this.heapSort()}>Heap Sort</button>
+//      <button onClick={() => this.heapSort()}>Heap Sort</button>
         <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
       </div>
     );
